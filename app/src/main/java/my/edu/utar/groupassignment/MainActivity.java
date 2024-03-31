5ad21f94e5b923e4c780f8523ab94287237a4242
tintinnnaa <tintin5008@gmail.com>
2024-03-31 14:11:51 +0800

    I make little changes only, you can make any changes

--- app/src/main/java/my/edu/utar/groupassignment/MainActivity.java
+++ app/src/main/java/my/edu/utar/groupassignment/MainActivity.java
@@ -2,13 +2,31 @@ package my.edu.utar.groupassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView switchPageTesting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        switchPageTesting = findViewById(R.id.switchPage);
+
+        switchPageTesting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pyDiscussion = new Intent(MainActivity.this, past_year_discussion.class);
+                pyDiscussion.putExtra("py", "PY Discussion");
+                startActivity(pyDiscussion);
+            }
+        });
+
     }
 }

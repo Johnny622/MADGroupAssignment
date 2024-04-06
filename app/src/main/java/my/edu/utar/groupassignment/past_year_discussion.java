95e186a1e0245015e30f2fc1b654d04f5da7477c
tintinnnaa <tintin5008@gmail.com>
2024-04-07 03:28:13 +0800

    updated on 6/4

--- app/src/main/java/my/edu/utar/groupassignment/past_year_discussion.java
+++ app/src/main/java/my/edu/utar/groupassignment/past_year_discussion.java
@@ -2,25 +2,6 @@ package my.edu.utar.groupassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import org.apache.commons.text.similarity.CosineSimilarity;
-import org.apache.commons.text.similarity.LevenshteinDistance;
-//import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
-//import org.deeplearning4j.models.word2vec.Word2Vec;
-//import org.deeplearning4j.models.word2vec.Word2Vec.Builder;
-//import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
-//import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
-//
-//import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
-//import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
-//import org.deeplearning4j.text.tokenization.tokenizer.Tokenizer;
-//import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
-//import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.TokenPreProcess;
-//import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
-//import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class past_year_discussion extends AppCompatActivity {
 
